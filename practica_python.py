eca7df45bf08de0d525b14750775a4bee2e292ad
miguelpuente <itpuentemiguelangel@gmail.com>
2024-07-17 09:26:58 -0300

    Forma 2 fibo y sumatoria con límite

--- practica_python.py
+++ practica_python.py
@@ -10,176 +10,183 @@
         - Convierte la lista en una tupla de tuplas.
 '''
 
-# # # Lista de productos y cantidades
-# # inventario = [("Manzanas", 50), ("Bananas", 30), ("Naranjas", 40), ("Peras", 25), ("Uvas", 60)]
+# # Lista de productos y cantidades
+# inventario = [("Manzanas", 50), ("Bananas", 30), ("Naranjas", 40), ("Peras", 25), ("Uvas", 60)]
 
-# # # Agregar un nuevo producto
-# # nuevo_produto = ("Kiwi", 20)
+# # Agregar un nuevo producto
+# nuevo_produto = ("Kiwi", 20)
 
-# # inventario.append(nuevo_produto)
+# inventario.append(nuevo_produto)
 
-# # # Encuentra e imprime el producto con la mayor cantidad disponible.
-# # producto_max = max(inventario, key=lambda x: x[1])
-# # print(f'Producto con mayor cantidad: {producto_max[0]} ({producto_max[1]} unidades)')
+# # Encuentra e imprime el producto con la mayor cantidad disponible.
+# producto_max = max(inventario, key=lambda x: x[1])
+# print(f'Producto con mayor cantidad: {producto_max[0]} ({producto_max[1]} unidades)')
 
-# # # Convierte la lista en una tupla de tuplas.
-# # inventario_tupla = tuple(inventario)
-# # print(f'Inventario (tupla): {inventario_tupla}')
+# # Convierte la lista en una tupla de tuplas.
+# inventario_tupla = tuple(inventario)
+# print(f'Inventario (tupla): {inventario_tupla}')
 
+#################################################################################################
+#################################################################################################
 
-# # '''
-# # Ejercicio: Sistema de Calificaciones
+'''
+    Diccionarios
 
-# #     Crea un diccionario con los nombres de tres estudiantes y sus respectivas calificaciones.
-# #     Agrega un nuevo estudiante y su calificación.
-# #     Calcula e imprime el promedio de las calificaciones.
-# #     Encuentra e imprime el estudiante con la calificación más alta.
-# # '''
+        Ejercicio: Sistema de Calificaciones
 
-# # # Diccionario de estudiantes y calificaciones
-# # estudiantes = {"Ana": 85, "Juan": 92, "Maria": 78}
+        - Crea un diccionario con los nombres de tres estudiantes y sus respectivas calificaciones.
+        - Agrega un nuevo estudiante y su calificación.
+        - Calcula e imprime el promedio de las calificaciones.
+        - Encuentra e imprime el estudiante con la calificación más alta.
+'''
 
-# # # Agregar un nuevo estudiante
-# # estudiantes["Carlos"] = 88
+# # Diccionario de estudiantes y calificaciones
+# estudiantes = {"Ana": 85, "Juan": 92, "Maria": 78}
 
-# # # Calcular el promedio de las calificaciones
-# # promedio = sum(estudiantes.values()) / len(estudiantes)
-# # print(f'Promedio de calificaciones: {promedio:.2f}')
+# # Agregar un nuevo estudiante
+# estudiantes["Carlos"] = 88
 
-# # # Encontrar el estudiante con la calificación más alta
-# # mejor_estudiante = max(estudiantes, key=estudiantes.get)
-# # print(f'Mejor estudiante: {mejor_estudiante} ({estudiantes[mejor_estudiante]} puntos)')
+# # Calcular el promedio de las calificaciones
+# promedio = sum(estudiantes.values()) / len(estudiantes)
+# print(f'Promedio de calificaciones: {promedio:.2f}')
 
+# # Encontrar el estudiante con la calificación más alta
+# mejor_estudiante = max(estudiantes, key=estudiantes.get)
+# print(f'Mejor estudiante: {mejor_estudiante} ({estudiantes[mejor_estudiante]} puntos)')
 
+#################################################################################################
+#################################################################################################
 
 ''''
-Ejercicio: Clasificación de Edad
+    Condicionales
 
-Solicita al usuario que ingrese su edad.
-Imprime una clasificación basada en la edad: 
+        Ejercicio: Clasificación de Edad
+
+        - Solicita al usuario que ingrese su edad.
+        - Imprime una clasificación basada en la edad: 
     
-    niño (0-12),
+            niño (0-12),
 
-    adolescente (13-17), 
+            adolescente (13-17), 
 
-    adulto (18-64), 
+            adulto (18-64), 
 
-    adulto mayor (65+).
-
-'''
-
-# # # Solicitar la edad al usuario
-# # edad = int(input("Ingrese su edad: "))
-
-# # # Clasificación basada en la edad
-# # if 0 < edad <= 12:
-# #     print('Clasificación: Niño')
-# # elif edad >=13 and edad <= 17:
-# #     print('Clasificación: Adolescente')
-# # elif edad >=18 and edad <= 64:
-# #     print('Clasificación: Adulto')
-# # elif edad >= 65:
-# #     print('Clasificación: Adulto Muyar')
-# # else:
-# #     print('Edad no válida')
-
-
-
-
-
-
-##################
-## Volvemos 20:25
-##################
+            adulto mayor (65+).
 
 '''
-Anidamiento de Condicionales
-    Ejercicio: Cálculo de Impuestos
 
-    Solicita al usuario que ingrese sus ingresos anuales.
-Calcula el impuesto a pagar según los siguientes tramos: 
+# # Solicitar la edad al usuario
+# edad = int(input("Ingrese su edad: "))
 
-    10% para ingresos hasta 10,000, 
-    20% para ingresos entre 10,001 y 20,000, 
-    30% para ingresos entre 20,001 y 30,000, 
-    y 40% para ingresos superiores a 30,000.
-'''
+# # Clasificación basada en la edad
+# if 0 < edad <= 12:
+#     print('Clasificación: Niño')
+# elif edad >=13 and edad <= 17:
+#     print('Clasificación: Adolescente')
+# elif edad >=18 and edad <= 64:
+#     print('Clasificación: Adulto')
+# elif edad >= 65:
+#     print('Clasificación: Adulto Muyar')
+# else:
+#     print('Edad no válida')
 
-# # # Solicitar los ingresos anuales al usuario
-# # ingresos = float(input("Ingresos anuales: "))
-
-
-# # # Cálculo del impuesto
-# # if ingresos <=10000:
-# #     impuesto = ingresos * 0.10
-# # elif ingresos <= 20000:
-# #     impuesto = 10000 * 0.10 + (ingresos - 10000) * 0.20
-# # elif ingresos <= 30000:
-# #     impuesto = 10000 * 0.10 + 10000 * 0.20 + (ingresos - 20000) * 0.30
-# # else:
-# #     impuesto = 10000 * 0.10 + 10000 * 0.20 + 10000 * 0.30 + (ingresos - 30000) * 0.40
-
-# # print(f'Impuesto a pagar: ${impuesto:.2f}')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#################################################################################################
+#################################################################################################
 
 '''
-Bucles For
-    Ejercicio: Serie Fibonacci
+        Ejercicio: Cálculo de Impuestos
 
-    Calcula e imprime los primeros 20 números de la serie Fibonacci.
+        - Solicita al usuario que introduzca sus ingresos anuales.
+        - Calcula el impuesto a pagar según los siguientes tramos: 
+
+            10% para ingresos hasta 10,000,
+
+            20% para ingresos entre 10,001 y 20,000,
+
+            30% para ingresos entre 20,001 y 30,000,
+
+            y 40% para ingresos superiores a 30,000.
 '''
 
-# Inicializar la serie Fibonacci
-fibonacci = [0, 1]
-
-# Calcular los primeros 20 números de la serie Fibonacci
-for i in range(2,20):
-    seguiente = fibonacci[i-1] + fibonacci[i-2]
-    #seguiente = fibonacci[i] + fibonacci[i-1]
-    fibonacci.append(seguiente)
-
-print(f'Serie Fibonacci: {fibonacci}')
+# # Solicitar los ingresos anuales al usuario
+# ingresos = float(input("Ingresos anuales: "))
 
 
+# # Cálculo del impuesto
+# if ingresos <= 10000:
+#     impuesto = ingresos * 0.10
+# elif ingresos <= 20000:
+#     impuesto = 10000 * 0.10 + (ingresos - 10000) * 0.20
+# elif ingresos <= 30000:
+#     impuesto = 10000 * 0.10 + 10000 * 0.20 + (ingresos - 20000) * 0.30
+# else:
+#     impuesto = 10000 * 0.10 + 10000 * 0.20 + 10000 * 0.30 + (ingresos - 30000) * 0.40
 
+# print(f'Impuesto a pagar: ${impuesto:.2f}')
 
-
-
+#################################################################################################
+#################################################################################################
 
 '''
-Bucles While
-Ejercicio: Sumatoria hasta un Límite
+    Bucles For
+        Ejercicio: Serie Fibonacci
 
-Solicita al usuario que ingrese números hasta que la suma de todos los números ingresados sea al menos 100.
-Imprime la suma total y la cantidad de números ingresados.
+        - Calcula e imprime los primeros 20 números de la serie Fibonacci.
+'''
+
+# ## Forma 1
+
+# # Inicializar la serie Fibonacci
+# fibonacci = [0, 1]
+
+# # Calcular los primeros 20 números de la serie Fibonacci
+# for i in range(2,20):
+#     siguiente = fibonacci[i-1] + fibonacci[i-2]
+#     fibonacci.append(siguiente)
+
+# print(f'Serie Fibonacci: {fibonacci}')
+
+########
+
+# ## Forma 2 (Taty)
+
+# # Inicializar la serie Fibonacci
+# fibonacci = [0, 1]
+
+# # Calcular los primeros 20 números de la serie Fibonacci
+# for i in range(1,19):
+#     siguiente = fibonacci[i] + fibonacci[i-1]
+#     fibonacci.append(siguiente)
+
+# print(f'Serie Fibonacci: {fibonacci}')
+
+#################################################################################################
+#################################################################################################
+
+'''
+    Bucles While
+        Ejercicio: Sumatoria hasta un Límite
+
+        - Solicita al usuario que ingrese números hasta que la suma de todos los números ingresados sea al menos 100.
+        - Imprime la suma total y la cantidad de números ingresados.
 '''
 
 
-# Inicializar la suma y el contador
-suma = 0
-contador = 0
+# # Inicializar la suma y el contador
+# suma = 0
+# contador = 0
 
-# Bucle while hasta que la suma sea al menos 100
+# # Bucle while hasta que la suma sea al menos 100
+# while suma < 100:
+#     numero = float(input("Ingrese un número: "))
+#     suma += numero
+#     contador += 1
 
+# # Imprimir la suma total y la cantidad de números ingresados
+# print(f"La suma total es: {suma}")
+# print(f"Se ingresaron {contador} números.")
 
-
-# Imprimir la suma total y la cantidad de números ingresados
-
-
-
+##################################################################################################################################################################################################
+##################################################################################################################################################################################################
 
 
