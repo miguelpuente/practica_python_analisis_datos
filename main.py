86e64ba4dbef0df2ae8c46154179f399c59346a2
Miguel Puente <wilo1985@hotmail.com>
2024-07-23 21:07:45 -0300

    Práctica laboratorio POO

---
+++ main.py
@@ -0,0 +1,20 @@
+
+def mostrar_menu():
+    print("========== Menú de Gestión de Colaboradores ==========")
+
+
+def agregar_colaborador(gestion, tipo_colaborador):
+    pass
+
+def buscar_colaborador_por_dni(gestion):
+    pass
+
+def actualizar_salario_colaborador(gestion):
+    pass
+
+def eliminar_colaborador_por_dni(gestion):
+    pass
+
+def mostrar_todos_los_colaboradores(gestion):
+    pass
+
